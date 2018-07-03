dad7aabdcaae04dfd0545e9e1ebd276fd4d680fe
Josiah Maina <josiah5115@gmail.com>
2018-07-02 21:15:49 -0600

    array list on state displayed on DOM

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: ['learn React',
+       'learn HTML/CSS', 
+       'learn SQL Database', 
+       'learn flex Box'],
+    };
+  }
+
+  // updateInput(value) {
+  //   this.setState({ list: value });
+  // }
+
   render() {
+    let arrayList = this.state.list.map((e, i) => {
+      return (
+        <h2 key={ i }>{ e } </h2>
+      )
+    });
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+        {/* <div className="App-header"> */}
+
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+          <h2>My React Practice Drills</h2>
+
+          { arrayList }
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       
+      // </div>
     );
   }
 }
-
 export default App;
